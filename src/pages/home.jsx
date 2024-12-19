dd56749ec197ea0460561e2cfe251be526af2507
RKB <balaji648balaji@gmail.com>
2024-12-19 17:27:13 +0530

    homepage half done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../homecomps/home.css";
 
 const Home = () => {
   return (
+
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <img src="" alt="logo" />
+      <h1 id='sname'>SQUAD 76</h1>
+      <h2 id='qs'>"Powered by ambition, defined by success."</h2>
       <nav>
         <ul>
           <li>
